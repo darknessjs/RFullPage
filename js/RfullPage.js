b457cea7baf3d8b7ee22b8cfdcce98b4226b272d
Darkness <616487222@qq.com>
2016-05-17 18:44:36 +0800

    新增横向banner滚动
    新增一个basic类，fullpage与bannerfullpage继承与basic
    暂无demo

--- js/RfullPage.js
+++ js/RfullPage.js
@@ -6,7 +6,7 @@
 		window.RfullPage=RfullPage;
 
 		jQuery.fn.setpagefunc=function(func){
-				this.animefun=func;
+			this.animefun=func;
 			return this;
 		};
 		jQuery.fn.setoutpagefunc=function(func){
@@ -25,30 +25,60 @@
 			}
 		}
 	}(window),
-	function(RfullPage){
-		RfullPage.RnewFullPage=function(navul){
+
+
+	function (RfullPage){
+		RfullPage.Rbasic=function(navul){
 			this.navul=null;
-			if(navul){
-				this.setnavul(navul);
-			}
-			this.pagenum=1;//初始化页数为1
-			this.divarr=[];
-			this.ismoving=false;
 			var pageobj=this;
-			this.topheight=0;
-			this.bottomheight=0;
 			this.setnavul=function(jquerydiv){
 				this.navul=jquerydiv;
 				jquerydiv.on("click","li",function(e){
 					pageobj.gotopage($(this).attr("data-page"));
 				});
 			};
+			if(navul){
+				this.setnavul(navul);
+			}
+			this.pagenum=1;//初始化页数为1
+			this.divarr=[];
+			this.ismoving=false;
+			this.ismousewhell=true;
 
-			this.addnav=function(navtext,pagenum){
+
+			this.setismousewhell=function(ismousewhell){
+				this.ismousewhell=ismousewhell;
+			};
+			this.navulactivecss="";
+			this.setnavulactivecss=function(cssname){
+				this.navulactivecss=cssname;
+			};
+			this.addnav=function(navtext,pagenum,isaddactive){
 				if(this.navul){
-					this.navul.append("<li data-page='"+pagenum+"'>"+navtext+"</li>")
+					this.navul.append("<li data-page='"+pagenum+"' data-isaddactive='"+(isaddactive!=null?isaddactive:'true')+"'>"+navtext+"</li>")
 				}
 			};
+
+
+			this.getpage=function(pagenum){//得到对应页的jquery对象
+				return this.divarr[(pagenum-1)];
+			};
+
+		}
+	}(RfullPage),
+
+	function(RfullPage){
+		RfullPage.RnewFullPage=function(navul){
+			var pageobj=this;
+			RfullPage.Rbasic.call(this,navul);
+
+			this.getfullpageheight=function(){
+				return document.documentElement.clientHeight;
+			};
+
+			this.topheight=0;
+			this.bottomheight=0;
+
 
 
 
@@ -60,60 +90,67 @@
 				this.bottomheight=jquerydiv.position().top;
 
 				jquerydiv.css("overflow","hidden");
-				jquerydiv.height(document.documentElement.clientHeight);
+				jquerydiv.height(this.getfullpageheight());
 				this.divarr.push(jquerydiv);
 				if(this.navul && title){
 					this.addnav(title,this.divarr.length)
 				}
 				return jquerydiv;
 			};
-			$("body").mousewheel(function(event, delta, deltaX, deltaY) {
-				if(deltaY>0 && !pageobj.ismoving){//向上滚
-					if(pageobj.bottomheight<(browsertop()-10)){//在底部
-						pageobj.gotopage(pageobj.divarr.length,true);
-						return false;
-					}else if(pageobj.pagenum!=1){//滚动条高度大于顶部的高度
-						pageobj.gotopage(pageobj.pagenum-1);
-						return false;
-					}
-				}else if(deltaY<0 && !pageobj.ismoving){//向下滚
-					if(browsertop()<(pageobj.topheight-(10))){ //在顶部时
-						pageobj.gotopage(1,true);
-						return false;
-					}else{//不在顶部
-						if(pageobj.pagenum!=pageobj.divarr.length){ //不在最后一个
-							pageobj.gotopage(pageobj.pagenum+1);
+			if(this.ismousewhell){
+				$("body").mousewheel(function(event, delta, deltaX, deltaY) {
+					if(deltaY>0 && !pageobj.ismoving){//向上滚
+						if(pageobj.bottomheight<(browsertop()-10)){//在底部
+							pageobj.gotopage(pageobj.divarr.length,true);
+							if(pageobj.navul){
+								pageobj.navul.stop().fadeIn("fast");
+							}
 							return false;
+						}else if(pageobj.pagenum!=1){//滚动条高度大于顶部的高度
+							pageobj.gotopage(pageobj.pagenum-1);
+							return false;
+						}else{
+							if(pageobj.navul){
+								pageobj.navul.stop().fadeOut("fast");
+							}
+						}
+					}else if(deltaY<0 && !pageobj.ismoving){//向下滚
+						if(browsertop()<(pageobj.topheight-(10))){ //在顶部时
+							pageobj.gotopage(1,true);
+							if(pageobj.navul){
+								pageobj.navul.stop().fadeIn("fast");
+							}
+							return false;
+						}else{//不在顶部
+							if(pageobj.pagenum!=pageobj.divarr.length){ //不在最后一个
+								pageobj.gotopage(pageobj.pagenum+1);
+								return false;
+							}else{
+								//if(pageobj.navul){
+								//	pageobj.navul.stop().fadeOut("fast");
+								//}
+							}
 						}
 					}
-				}
-				if(pageobj.ismoving){
-					return false;
-				}
-			});
-			this.getpage=function(pagenum){//得到对应页的jquery对象
-				return this.divarr[(pagenum-1)];
-			};
+					if(pageobj.ismoving){
+						return false;
+					}
+				});
+			}
 			this.gotopage=function(pagenum,isskipanime){//跳转到指定页
 				if(this.getpage(this.pagenum).outanimefun && this.pagenum!=pagenum){
 					this.getpage(this.pagenum).outanimefun();
 				}
-
-
-			if(this.navul){
-				this.navul.children("li").each(function(i,e){
-					if($(this).attr("data-page")==pagenum){
-						$(this).addClass("navul_li_active");
-					}else{
-						$(this).removeClass("navul_li_active");
-					}
-				});
-			}
-
-
-
+				if(this.navul){
+					this.navul.children("li").each(function(i,e){
+						if($(this).attr("data-page")==pagenum && $(this).attr("data-isaddactive")=="true"){
+							$(this).addClass(pageobj.navulactivecss);
+						}else{
+							$(this).removeClass(pageobj.navulactivecss);
+						}
+					});
+				}
 				this.pagenum=parseInt(pagenum);
-
 				this.ismoving=true;
 				if(!isskipanime){
 					isskipanime=false;
@@ -121,21 +158,78 @@
 				if(this.getpage(pagenum).animefun && !isskipanime){
 					this.getpage(pagenum).animefun();
 				}
-				$("html,body").stop().animate({scrollTop:(pagenum-1)*document.documentElement.clientHeight+pageobj.topheight},"2000",function(e){
+				$("html,body").stop().animate({scrollTop:(pagenum-1)*pageobj.getfullpageheight()+pageobj.topheight},"2000",function(e){
 					pageobj.ismoving=false;
-			})
+				});
 			};
-
 			$(window).bind("resize",function(e) {
+				if(pageobj.getpage(pageobj.pagenum).animefun){
+					pageobj.getpage(pageobj.pagenum).animefun();
+				}
 				$.each(pageobj.divarr,function(i,e){
 					if(i==0){
 						pageobj.topheight=e.position().top;
 					}
 					pageobj.bottomheight=e.position().top;
-					e.height(document.documentElement.clientHeight);
+					e.height(pageobj.getfullpageheight());
 					pageobj.gotopage(pageobj.pagenum,true);
 				})
 			});
 		}
 
-	}(RfullPage)
+	}(RfullPage),
+
+	function(RfullPage){
+		RfullPage.RnewBannerFullPage=function(parentjqueryul,navul){
+			var pageobj=this;
+			RfullPage.Rbasic.call(this,(navul?navul:null));
+			parentjqueryul.css("position","relative");
+			parentjqueryul.children("li").each(function(i,e){
+				if(navul){
+					navul.append("<li data-page='"+(i-(-1))+"'></li>")
+				}
+				pageobj.divarr.push($(this));
+				$(this).css("width","100%").css("height","100%").css("left",i*(parentjqueryul.width())+"px").css("top","0px").css("position","absolute");
+				if(i!=0){
+					$(this).animate({opacity:"0"});
+				}
+			});
+
+			if(navul){
+				navul.on("click","li",function(e){
+					pageobj.gotopage($(this).attr("data-page"));
+				});
+			}
+
+
+			this.gotopage=function(pagenum){
+				navul.children("li:eq("+(this.pagenum-1)+")").removeClass(this.navulactivecss);
+				navul.children("li:eq("+(pagenum-1)+")").addClass(this.navulactivecss);
+				if(pagenum>this.pagenum){
+					pageobj.divarr[pagenum-1].css("left",100+"px").stop().animate({opacity:"1",left:0},"slow");
+					pageobj.divarr[this.pagenum-1].stop().animate({opacity:"0",left:-100},"slow");
+				}else if(pagenum<this.pagenum){
+					pageobj.divarr[pagenum-1].css("left",-(100)+"px").stop().animate({opacity:"1",left:0},"slow");
+					pageobj.divarr[this.pagenum-1].stop().animate({opacity:"0",left:100},"slow");
+				}else{
+					return false;
+				}
+				this.pagenum=pagenum;
+				this.startanime();
+				//this.animet=window.setInterval(function(){pageobj.gotopage((pageobj.divarr.length==pageobj.pagenum?1:(pageobj.pagenum-(-1))))},6000);
+			};
+			this.isanime=true;
+			this.animespeed=5000;
+			this.startanime=function(){
+				if(this.isanime){
+					if(this.animet)
+						window.clearInterval(this.animet);
+					this.animet=window.setInterval(function(){pageobj.gotopage((pageobj.divarr.length==pageobj.pagenum?1:(pageobj.pagenum-(-1))))},5000);
+				}
+			}
+
+
+
+
+		}
+	}(RfullPage);
